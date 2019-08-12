ca3d19fe8e32f17a4fb836799087a419742c0f7d
stilyss <stilyssk@gmail.com>
2019-08-12 21:25:41 +0300

    ExamRetake/

--- 07ReflectionAndAttributesLab/Stealer/Spy.cs
+++ 07ReflectionAndAttributesLab/Stealer/Spy.cs
@@ -2,33 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace Stealer
-{
+
     public class Spy
     {
         public string StealFieldInfo(string investigatedClass,params string[] fields)
         {
-            
-            var type = Type.GetType("Stealer.Hacker");
-            //FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public|BindingFlags.ExactBinding)
-            FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public|BindingFlags.ExactBinding);
+            var namespaceName = GetType().Namespace;
+            var type = Type.GetType(investigatedClass);
+            FieldInfo[] fieldInfos = type.
+                GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.ExactBinding)
+                .Where(f => f.GetCustomAttribute<CompilerGeneratedAttribute>() == null)
+                .ToArray();
+            Object classInstance = Activator.CreateInstance(type, new object[] { });
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(Print(investigatedClass));
+
             foreach (var field in fieldInfos)
             {
-                Console.WriteLine(field.Name);
+                var name = field.Name;
+                var value = field.GetValue(classInstance).ToString();
+                result.AppendLine(PringFieldInformation(name, value));
             }
-
-            Object classInstance = Activator.CreateInstance(type, new object[] { });
-
-
-            //string printString = this.Print("nameOfTheClass");
-            //string fieldInformationString = this.PringFieldInformation("fieldName", "fieldValue");
-            //StringBuilder result = new StringBuilder();
-            //result.Append(printString);
-            //result.Append(fieldInformationString);
-            //return result.ToString();
-            return null;
+            return result.ToString();
         }
 
         private string Print(string nameOfTheClass)
@@ -41,4 +39,4 @@ namespace Stealer
             return $"{ filedName} = { fieldValue}";
         }
     }
-}
+
